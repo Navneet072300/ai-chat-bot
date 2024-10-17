92927807b5746f829d5a326abfb92c36a861e958
Navneet Shahi <navneetshahi345@gmail.com>
2024-10-17 19:44:18 +0530

    firest commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import { MainPage } from "@/components/Home";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <MainPage />
+      <p>Home</p>
+    </main>
+  );
+}
